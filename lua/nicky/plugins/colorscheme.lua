b90d1f803e0b82e730dff53c386ce7d9c0470f99
Nicky Meuleman <nicky.dev@outlook.com>
2023-05-10 22:12:22 +0200

    a different theme does not fix the weird invisible text issue

--- lua/nicky/plugins/colorscheme.lua
+++ lua/nicky/plugins/colorscheme.lua
@@ -1,6 +1,12 @@
+-- local M = {
+--   "bluz71/vim-nightfly-colors",
+--   name = "nightfly",
+--   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+-- }
 local M = {
-  "bluz71/vim-nightfly-colors",
-  name = "nightfly",
+  "catppuccin/nvim",
+  name = "catppuccin",
   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
 }
