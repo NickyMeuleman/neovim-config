d949c769b645cf5fbbf24b559d7418cdb37bb3d3
Nicky Meuleman <nicky.dev@outlook.com>
2023-05-19 21:18:53 +0200

    back to nightfly colorscheme

--- lua/nicky/plugins/colorscheme.lua
+++ lua/nicky/plugins/colorscheme.lua
@@ -1,22 +1,18 @@
--- local M = {
--- eou  "bluz71/vim-nightfly-colors",
---   name = "nightfly",
---   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
--- }
 local M = {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    -- "catppuccin/nvim",
+    -- name = "catppuccin",
+    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
 }
 
 -- load colorscheme by executing a vim command
 function M.config()
-  local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
-  if not status_ok then
-    return
-  end
+    local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
+    if not status_ok then
+        return
+    end
 end
 
 -- return the plugin spec lua table lazy.nvim uses
