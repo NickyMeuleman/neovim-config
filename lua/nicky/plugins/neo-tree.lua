5dc206308c86072406c4b608650b3e48606fa33b
Nicky Meuleman <nicky.dev@outlook.com>
2023-06-03 20:21:24 +0200

    use neotree

--- lua/nicky/plugins/neo-tree.lua
+++ lua/nicky/plugins/neo-tree.lua
@@ -1,3 +1,5 @@
+-- VSCode file colours in explorer:
+-- lightblue
 -- "gitDecoration.modifiedResourceForeground": "#a2bffc",
 -- red
 -- "gitDecoration.deletedResourceForeground": "#EF535090",
@@ -7,6 +9,7 @@
 -- "gitDecoration.ignoredResourceForeground": "#395a75",
 -- yellow/pink
 -- "gitDecoration.conflictingResourceForeground": "#ffeb95cc",
+
 local M = {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v2.x",
@@ -23,6 +26,9 @@ function M.config()
 		return
 	end
 
+	-- Unless you are still migrating, remove the deprecated commands from v1.x
+	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
 	-- If you want icons for diagnostic errors, you'll need to define them somewhere:
 	vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
 	vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
@@ -32,8 +38,21 @@ function M.config()
 	neo_tree.setup({
 		close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
 		window = {
-			position = "right",
+			position = "right", -- right side, correct side
 		},
+		source_selector = {
+			winbar = true,
+			sources = {
+				{ source = "filesystem", display_name = " 󰉓 Files " },
+				{ source = "git_status", display_name = " 󰊢 Git " },
+			},
+		},
+		filesystem = {
+			filtered_items = {
+				visible = true, -- show dotfiles and gitignored files
+			},
+		},
+		-- adding icons to be compatible with Nerdfont v3
 		default_component_configs = {
 			icon = {
 				folder_empty = "󰜌",
@@ -66,34 +85,16 @@ function M.config()
 				StaticMethod = { icon = "󰠄 ", hl = "Function" },
 			},
 		},
-		-- Add this section only if you've configured source selector.
-
-		source_selector = {
-			winbar = true,
-			sources = {
-				{ source = "filesystem", display_name = " 󰉓 Files " },
-				{ source = "git_status", display_name = " 󰊢 Git " },
-			},
-		},
 	})
-	vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+	-- Tweak highlight groups
+	-- To see all highlight groups :Telescope highlights
+	-- because untracked files are usually new files
 	vim.api.nvim_set_hl(0, "NeoTreeGitUntracked", { link = "NeoTreeGitAdded" })
+	-- dotfiles and ignored files are a bit hard to read out of the box
+	vim.api.nvim_set_hl(0, "NeoTreeDotfile", { link = "NeoTreeDimText" })
+	-- the confirmation prompts are unreadably in nightfly
+	vim.api.nvim_set_hl(0, "NeoTreeTitleBar", { link = "NeoTreePreview" })
 end
 
 return M
-
---[[ source_selector provides clickable tabs to switch between sources.
-  source_selector = {
-    winbar = false, -- toggle to show selector on winbar
-    statusline = false, -- toggle to show selector on statusline
-      filtered_items = {
-      visible = false, -- when true, they will just be displayed differently than normal items
-      force_visible_in_empty_folder = false, -- when true, hidden files will be shown if the root folder is otherwise empty
-      show_hidden_count = true, -- when true, the number of hidden items in each folder will be shown as the last entry
-      hide_dotfiles = true,
-      hide_gitignored = true,
-      hide_hidden = true, -- only works on Windows for hidden files/directories
-      hide_by_name = {
-        ".DS_Store",
-        "thumbs.db"
-        --"node_module ]]
